a894f66a2492f97c3cd801c962746447ccf188e3
857045967 <LiaoDiao>
2015-05-20 14:55:46 +0800

    321

---
+++ teslDeom1/teslDeom1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  teslDeom1
+//
+//  Created by 周建 on 15/5/20.
+//  Copyright (c) 2015年 周建. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ teslDeom1/teslDeom1/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  teslDeom1
+//
+//  Created by 周建 on 15/5/20.
+//  Copyright (c) 2015年 周建. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
